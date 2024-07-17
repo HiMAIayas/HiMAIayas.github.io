0af0f14b3dba17b2749b1fbc175c0be8bd75bc47
Phumipas Namjaidee <phumihimal@gmail.com>
2024-07-18 04:56:19 +0700

    change layout

--- src/app/components/WrapperDropdown.tsx
+++ src/app/components/WrapperDropdown.tsx
@@ -10,14 +10,14 @@ interface ChildrenProps {
 export default function WrapperDropdown({ children, topic }: ChildrenProps) {
     const [isOpen, setIsOpen] = useState(true);
     return (
-        <div>
+        <div className='my-3 rounded-md border-gray-500 border-solid border-b-2'>
             
-            <div className='flex justify-center items-center group'>
+            <div className='flex justify-center items-center group mb-2'>
                 
                 <div className='hidden group-hover:block'>
                     <img className='w-40' src="/arrow.png" alt="arrow"></img>
                 </div>
-                <div className='text-2xl font-bold px-5 py-3 rounded-md border-slate-50 border-2 group-hover:bg-gray-100 group-hover:border-gray-700 cursor-pointer'
+                <div className='text-2xl font-bold px-5 py-3 rounded-md   group-hover:shadow-md cursor-pointer'
                     onClick={() => setIsOpen(!isOpen)}>
                     {topic}
                 </div>
@@ -26,9 +26,11 @@ export default function WrapperDropdown({ children, topic }: ChildrenProps) {
                 </div>
             </div>
 
-            <div className={`w-full border-gray-950 border-solid border-2 ${isOpen ? "flex" : "hidden"}`}>
+            <div className={`w-full  ${isOpen ? "flex" : "hidden"}`}>
                 {children}
             </div>
+            
+
 
         </div>
     )
